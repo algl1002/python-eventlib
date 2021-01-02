bc942466dd3f05e770b094d48dade2071583f176
algl1002 <algljob@gmail.com>
2021-01-03 01:17:20 +0300

    clock() function of time module is deprecated

--- test_job.py
+++ test_job.py
@@ -6,10 +6,10 @@
 #     print('%d. Sleep .1 second' % i)
 #     i = i + 1
 
-from time import clock
+from time import perf_counter
 from eventlib.green.socket import gethostbyname
 
-st = clock()
+st = perf_counter()
 for i in range(100):
     print(gethostbyname('ag-projects.com'))
-print('Time used: {}s'.format(clock() - st))
+print('Time used: {}s'.format(perf_counter() - st))
